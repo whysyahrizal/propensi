6079acadbdf88d91c861b1a9cb9b245ad322c7b2
Yoshelin Yamala Vijnana <yoshelin.yamala@ui.ac.id>
2026-03-08 05:27:04 +0700

    fill the admin, forms, models, urls, views, etc

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('personel_list')),
     path('admin/', admin.site.urls),
+    path('personel/', include('personel.urls')),
 ]
--- personel/admin.py
+++ personel/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Unit, Personel
 
-# Register your models here.
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nama')
+    search_fields = ('nama',)
+
+
+@admin.register(Personel)
+class PersonelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nama', 'nip', 'unit', 'is_active')
+    list_filter = ('unit', 'is_active')
+    search_fields = ('nama', 'nip')
