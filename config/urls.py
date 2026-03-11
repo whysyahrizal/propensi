63357d12c36f4801a7d30f3bd9e37cd3d2b5e6f8
Sekar Gandes Dianti <sekar.gandes@ui.ac.id>
2026-03-11 13:35:38 +0300

    Fix SPRIN detail: rejection note, description display, dropped GPS fields, media URL serving + response handling

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.shortcuts import redirect
 
 urlpatterns = [
     path('', lambda request: redirect('/personel/')),
     path('personel/', include('personel.urls')),
+    path('sprin/', include('sprin.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ sprin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SprinConfig(AppConfig):
+    name = 'sprin'
