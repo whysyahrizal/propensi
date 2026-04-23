27e0d43e7ff260a214d7525d39ffdfab573c4d3e
Your Name <you@example.com>
2026-04-23 13:22:02 +0700

    edit build.sh

--- schedules/migrations/0001_initial.py
+++ schedules/migrations/0001_initial.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('dashboard', '0001_initial'),
+        ('locations', '0001_initial'),
     ]
 
     operations = [
